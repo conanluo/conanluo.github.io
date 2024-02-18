e7a4d3e185c6d7d12402b8f413567d42eae58305
conanluo <conanluo@163.com>
2024-02-18 12:26:44 -0800

    testting
    testing

---
+++ ioa/dar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CREATE BY DAR</title>
+    <script src="./js/jq.js"></script>
+    <script src="./js/newdar.js"></script>
+    <style>
+        *{
+            font-size: 12pt;
+        }
+        #pasteArea{
+            width: 100%;
+            height: 60px;
+        }
+        
+    </style>
+</head>
+<body>
+    <textarea name="pasteArea" id="pasteArea"></textarea>
+    <div class="footer"></div>
+</body>
+</html>
---
+++ ioa/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="font-size:30px; ">
+        <div style="background: lightgreen;height:100px">
+             <a href="dar.php">CREATE NEXT DAY DAR</a>
+        </div>
+
+
+<hr>
+        <img src="./img/1.png" style="width: 1000px"/><hr>
+        <img src="./img/2.png" style="width: 1000px"/><hr>
+        <img src="./img/3.png" style="width: 1000px"/><hr>
+        <img src="./img/4.png" style="width: 1000px"/><hr>
+        <img src="./img/5.png" style="width: 1000px"/><hr>
+        <img src="./img/6.png" style="width: 1000px"/><hr>
+    </div>
+</body>
+</html>
